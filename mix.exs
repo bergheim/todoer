ce2d27265f1b5cf4eac8c2e15b414d84fa737470
Thomas Bergheim <knighthk@glvortex.net>
2024-06-25 12:56:50 +0200

    reload iex and tests on save

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule Todoer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:exsync, "~> 0.4.1", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
