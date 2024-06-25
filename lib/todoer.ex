e0f1e193094ed5cb584c771e44f534db3f548410
Thomas Bergheim <knighthk@glvortex.net>
2024-06-25 12:57:39 +0200

    add initial wip todo list implementation

--- lib/todoer.ex
+++ lib/todoer.ex
@@ -15,4 +15,20 @@ defmodule Todoer do
   def hello do
     :world
   end
+
+  defstruct next_id: 1, entries: %{}
+  def new(), do: %Todoer{}
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.next_id)
+    new_entries = Map.put(todo_list.entries, todo_list.next_id, entry)
+
+    %Todoer{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Map.values()
+    |> Enum.filter(fn entry -> entry.date == date end)
+  end
 end
--- test/todoer_test.exs
+++ test/todoer_test.exs
@@ -5,4 +5,25 @@ defmodule TodoerTest do
   test "greets the world" do
     assert Todoer.hello() == :world
   end
+
+  test "a new todo list is empty" do
+    assert Todoer.new() == %Todoer{}
+  end
+
+  test "can add a new entry" do
+    todo_list =
+      Todoer.new()
+      |> Todoer.add_entry(%{date: ~D[2024-01-01], title: "Some"})
+      |> Todoer.add_entry(%{date: ~D[2024-10-01], title: "kind"})
+      |> Todoer.add_entry(%{date: ~D[2024-11-01], title: "of"})
+      |> Todoer.add_entry(%{date: ~D[2024-12-01], title: "blue"})
+
+    assert Todoer.entries(todo_list, ~D[2024-01-01]) == [
+             %{
+               date: ~D[2024-01-01],
+               title: "Some",
+               id: 1
+             }
+           ]
+  end
 end
